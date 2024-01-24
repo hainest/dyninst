bd8dc2b374580b08050cddec855149b3441741da
Tim Haines <thaines.astro@gmail.com>
2024-01-24 01:04:11 -0600

    Add stackwalk/addrRange.h

--- stackwalk/src/addrRange.h
+++ stackwalk/src/addrRange.h
@@ -31,24 +31,11 @@
 #ifndef _addrRange_h_
 #define _addrRange_h_
 
-/*******************************************************/
-/*	Templated header file                               */
-/*******************************************************/
-
 #include <assert.h>
 #include <stdlib.h>
 #include <string>
 #include <vector>
 #include "dyntypes.h"
-
-/** template class for addrRangeTree. The implementation is based on red black
- * tree implementation for efficiency concerns and for getting sorted
- * elements easier.
- * There are two template types, K (key) and V (value).
- */
-
-/* Note: this is a near copy of BPatch_Set. That class didn't do what I needed,
-   so... -- bernat, 10OCT03 */
 
 typedef enum { TREE_RED, TREE_BLACK } color_t;
 
@@ -63,70 +50,47 @@ class addrRange {
    }
 };
 
-/** 
- * T should inherit from addrRange
- **/
 template <class T>
 class addrRangeTree {
 
-   /** tree implementation structure. Used to implement the RB tree */
    typedef struct entry {
       Dyninst::Address key;
       T *value;
-      color_t color;	/* color of the node */
-      struct entry* left; /* left child */
-      struct entry* right; /* right child */
-      struct entry* parent; /* parent of the node */
+      color_t color;
+      struct entry* left;
+      struct entry* right;
+      struct entry* parent;
 
-      /** constructor for structure */
       entry() 
          : key(0), value(NULL), color(TREE_BLACK),
            left(NULL), right(NULL), parent(NULL)
       {
       }
 
-      /** constructor used for non-nil elements 
-       * @param e nil entry
-       */	  
-      entry(entry* e) //constructor with nil entry 
+      entry(entry* e)
          : key(0), value(NULL), color(TREE_RED),
            left(e), right(e), parent(NULL)
       {
       }
 
-      /** constructor
-       * @param d data element
-       * @param e nill entry 
-       */
       entry(Dyninst::Address key_, T *value_, entry* e)
          : key(key_), value(value_), color(TREE_RED), left(e),
            right(e), parent(NULL) 
       {
       }
 
-      /** constructor 
-       * @param e the entry structure that will be copied 
-       */
       entry(const entry& e) : key(e.key),value(e.value),color(e.color),
                               left(NULL),right(NULL),parent(NULL) 
       {
       }
    } entry;
 
-   /** pointer to define the nil element of the tree NULL is not used
-    * since some operations need sentinel nil which may have non-nil
-    * parent.
-    */
    entry* nil;
 
-   /** size of the tree */
    int setSize;
 
-   /** pointer to the tree structure */
    entry* setData;
 
-   // method that implements left rotation used by RB tree for balanced
-   // tree construction and keeps the RBtree properties.
    void leftRotate(entry* pivot)
    {
       if(!pivot || (pivot == nil))
@@ -149,8 +113,6 @@ class addrRangeTree {
       pivot->parent = y;
    }
 
-   // method that implements right rotattion used by RB tree for balanced
-   // tree construction and keeps the RBtree properties.
    void rightRotate(entry *pivot)
    {
       if(!pivot || (pivot == nil))
@@ -173,8 +135,6 @@ class addrRangeTree {
       pivot->parent = x;
    }
 
-   // method that modifies the tree structure after deletion for keeping
-   // the RBtree properties.
    void deleteFixup(entry *x)
    {
       while((x != setData) && 
@@ -239,8 +199,6 @@ class addrRangeTree {
    }
 
 
-   // insertion to a binary search tree. It returns the new element pointer
-   // that is inserted. If element is already there it returns NULL
    entry* treeInsert(Dyninst::Address key, T *value)
    {
       entry* y = NULL;
@@ -270,9 +228,6 @@ class addrRangeTree {
    }
 
 
-   // finds the elemnts in the tree that will be replaced with the element
-   // being deleted in the  deletion. That is the element with the largest
-   // smallest value than the element being deleted. 
    entry* treeSuccessor(entry *x) const
    {
       if(!x || (x == nil))
@@ -290,8 +245,6 @@ class addrRangeTree {
       return y;
    }
 
-   // method that returns the entry pointer for the element that is searched
-   //for. If the entry is not found then it retuns NULL
    entry* find_internal(Dyninst::Address element) const
    {
       entry* x = setData;
@@ -309,7 +262,6 @@ class addrRangeTree {
    }
 
 
-   // infix traverse of the RB tree. It traverses the tree in ascending order
    void traverse(T **all, entry *node, int &n) const
    {
       if(node == nil)
@@ -323,8 +275,6 @@ class addrRangeTree {
    }
 
 
-   // Vector version of above
-   // infix traverse of the RB tree. It traverses the tree in ascending order
    void traverse(std::vector<T *> &all, entry*node) const
    {
       if(node == nil)
@@ -337,7 +287,6 @@ class addrRangeTree {
    }
 
 
-   // deletes the tree structure for deconstructor.
    void destroy(entry *node)
    {
       if(!node || (node == nil))
@@ -349,12 +298,10 @@ class addrRangeTree {
       delete node;
    }
 
-   /** copy constructor */
    addrRangeTree(const addrRangeTree &/* y */) 
    {
    }
 
-   // Similar to precessor, but returns an entry
    bool precessor_internal(Dyninst::Address key, entry * &value) const
    {
       entry *x = setData;
@@ -388,7 +335,6 @@ class addrRangeTree {
    }
 
 
-   // Similar to successor, but returns an entry
    bool successor_internal(Dyninst::Address key, entry * &value) const
    {
       entry *x = setData;
@@ -422,7 +368,6 @@ class addrRangeTree {
 
  public:
 
-   /** constructor. The default comparison structure is used */
    addrRangeTree() : 
       setSize(0) 
    { 
@@ -430,28 +375,22 @@ class addrRangeTree {
       setData = nil;
    }
     
-      /** destructor which deletes all tree structure and allocated entries */
       virtual ~addrRangeTree()
       {
          destroy(setData);
          delete nil;
       }
 
-      /** returns the cardinality of the tree , number of elements */
       int size() const 
       { 
          return setSize; 
       }
     
-      /** returns true if tree is empty */
       bool empty() const 
       { 
          return (setData == nil); 
       }
 
-      /** inserts the element in the tree 
-       * @param 1 element that will be inserted
-       */
       void insert(T *value)
       {
          entry* x = treeInsert(value->get_address(), value);
@@ -501,9 +440,6 @@ class addrRangeTree {
          setData->color = TREE_BLACK;
       }
 
-      /** removes the element in the tree 
-       * @param 1 element that will be removed  
-       */
       void remove(Dyninst::Address key)
       {
          entry* z = find_internal(key);
@@ -533,9 +469,6 @@ class addrRangeTree {
       }
 
 
-      /** returns true if the argument is member of the addrRangeTree
-       * @param e the element that will be searched for
-       */
       virtual bool find(Dyninst::Address key, T *& value) const
       {
          value = NULL;
@@ -556,9 +489,6 @@ class addrRangeTree {
       }
 
 
-      /** Fills in the vector with all address ranges that overlap
-       * with the address range defined by (start, end]
-       */
       virtual bool find(Dyninst::Address start, Dyninst::Address end,
                         std::vector<T *> &ranges) const
       {
@@ -581,10 +511,6 @@ class addrRangeTree {
          return (ranges.size() != 0);
       }
 
-
-      /** Returns the largest value less than or equal to the
-       * key given
-       */
       virtual bool precessor(Dyninst::Address key, T *& value) const
       {
          entry *val;
@@ -596,9 +522,6 @@ class addrRangeTree {
       }
 
 
-      /** Returns the smallest value greater than or equal to the
-       * key given
-       */
       virtual bool successor(Dyninst::Address key, T *& value) const
       {
          entry *val;
@@ -609,11 +532,6 @@ class addrRangeTree {
          return true;
       }
 
-      /** fill an buffer array with the sorted
-       * elements of the addrRangeTree in ascending order according to comparison function
-       * if the addrRangeTree is empty it retuns NULL, other wise it returns 
-       * the input argument.
-       */
       T ** elements(T ** buffer) const
       {
          if(setData == nil) return NULL;
@@ -624,7 +542,6 @@ class addrRangeTree {
       }
 
 
-      // And vector-style
       bool elements(std::vector<T *> &buffer) const
       {
          if(setData == nil) return false;
@@ -632,7 +549,6 @@ class addrRangeTree {
          return true;
       }
 
-      // Remove all entries in the tree
       void clear()
       {
          if (setData == nil) return;
