a1b781cec9896f2aa33ffb4f613e7e0a4e8aaa5f
Tim Haines <thaines.astro@gmail.com>
2024-02-12 21:56:31 -0600

    Fix use of Instruction::Ptr

--- dataflowAPI/src/ExpressionConversionVisitor.C
+++ dataflowAPI/src/ExpressionConversionVisitor.C
@@ -355,8 +355,6 @@ SgAsmExpression *ExpressionConversionVisitor::makeSegRegExpr() {
     }
 }
 
-/////////////// Visitor class /////////////////
-
 void ExpressionConversionVisitor::visit(BinaryFunction *binfunc) {
     assert(m_stack.size() >= 2);
     SgAsmExpression *rhs = m_stack.front();
