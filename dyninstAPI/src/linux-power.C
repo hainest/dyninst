432398665535c983f676df4597d46a0d65c6a67d
Tim Haines <thaines.astro@gmail.com>
2024-03-11 17:04:40 -0500

    Add dyninstAPI/src/linux-power.C

--- dyninstAPI/src/linux-power.C
+++ dyninstAPI/src/linux-power.C
@@ -199,8 +199,6 @@ bool AddressSpace::getDyninstRTLibName() {
     return true;
 }
 
-// floor of inferior malloc address range within a single branch of x
-// for 32-bit ELF PowerPC mutatees
 Dyninst::Address region_lo(const Dyninst::Address x) {
    const Dyninst::Address floor = getpagesize();
 
@@ -213,8 +211,6 @@ Dyninst::Address region_lo(const Dyninst::Address x) {
 }
 
 
-// floor of inferior malloc address range within a single branch of x
-// for 64-bit ELF PowerPC mutatees
 Dyninst::Address region_lo_64(const Dyninst::Address x) {
    const Dyninst::Address floor = getpagesize();
 
@@ -226,9 +222,6 @@ Dyninst::Address region_lo_64(const Dyninst::Address x) {
    return floor;
 }
 
-
-// ceiling of inferior malloc address range within a single branch of x
-// for 32-bit ELF PowerPC mutatees
 Dyninst::Address region_hi(const Dyninst::Address x) {
    const Dyninst::Address ceiling = ~(Dyninst::Address)0 & 0xffffffff;
 
@@ -241,8 +234,6 @@ Dyninst::Address region_hi(const Dyninst::Address x) {
 }
 
 
-// ceiling of inferior malloc address range within a single branch of x
-// for 64-bit ELF PowerPC mutatees
 Dyninst::Address region_hi_64(const Dyninst::Address x) {
    const Dyninst::Address ceiling = ~(Dyninst::Address)0;
 
