4987acce51a2be9e772b329172323402a1352957
Tim Haines <thaines.astro@gmail.com>
2024-10-29 09:27:11 -0500

    Remove Interrupt Vector Offset Register
    7.2.15 Interrupt Vector Offset Register, ISA v2.07, Book III
    [Category: Embedded.Phased-Out]
    
    This can never be referred to by name and is a privileged register.
    Rather, it is implicitly used by 'mfspr	r4, 0x3f'. They are not part of
    the ABI, so no pseudoregister is needed.

--- instructionAPI/capstone/ppc/registers.py
+++ instructionAPI/capstone/ppc/registers.py
@@ -64,7 +64,6 @@ _dyninst_special = [
 ]
 _dyninst_dead = [
   # Not directly addressable. Implicitly read/written via m{f,t}spr instructions (ISA v2.01)
-  "ivor8",    # 5.2.10 Interrupt Vector Offset Registers, ISA v2.03 (only in v2.0{3,4,5,6,7})
   "ppr",      # 3.2.3 Program Priority Register
   "ppr32",    # 3.2.3 Program Priority Register (upper 32 bits of PPR)
 ]
