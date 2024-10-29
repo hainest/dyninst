3db0bd7faf91692faf81d0247eb3980a3a87e5ec
Tim Haines <thaines.astro@gmail.com>
2024-10-29 09:14:03 -0500

    Remove Machine State Register
    3.2.1 Machine State Register, ISA v2.07, Book III
    
    This can never be referred to by name. I couldn't find any explicit
    note about its privilege requirements, but I'm assuming it is.
    It is implicitly used by instructions like 'mfmsr r4'. It is not part
    of the ABI, so no pseudoregister is needed.

--- instructionAPI/capstone/ppc/registers.py
+++ instructionAPI/capstone/ppc/registers.py
@@ -64,7 +64,6 @@ _dyninst_special = [
 ]
 _dyninst_dead = [
   # Not directly addressable. Implicitly read/written via m{f,t}spr instructions (ISA v2.01)
-  "msr",      # 2.2.3 Machine State Register
   "ivpr",     # 5.2.8 Interrupt Vector Prefix Register, ISA v2.03 (only in v2.0{3,4,5,6,7})
   "ivor8",    # 5.2.10 Interrupt Vector Offset Registers, ISA v2.03 (only in v2.0{3,4,5,6,7})
   "ppr",      # 3.2.3 Program Priority Register
