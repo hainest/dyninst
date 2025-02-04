cc53c8fbc3f2027f1c5278e8252d0900510c88bc
Tim Haines <thaines.astro@gmail.com>
2025-02-04 00:26:23 -0600

    constructInitialFrame

--- parseAPI/src/IA_aarch64.C
+++ parseAPI/src/IA_aarch64.C
@@ -28,230 +28,203 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include "common/src/arch.h"
 #include "IA_aarch64.h"
 #include "instructionAPI/h/syscalls.h"
-#include "common/src/arch.h"
-#include "registers/aarch64_regs.h"
 #include "parseAPI/src/debug_parse.h"
-
-#include <deque>
-#include <iostream>
-#include <sstream>
-#include <functional>
-#include <algorithm>
-#include <set>
 #include "Register.h"
+#include "registers/aarch64_regs.h"
+
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <set>
+#include <sstream>
 
 using namespace Dyninst;
 using namespace InstructionAPI;
 using namespace Dyninst::ParseAPI;
 using namespace Dyninst::InsnAdapter;
 
-IA_aarch64::IA_aarch64(Dyninst::InstructionAPI::InstructionDecoder dec_,
-               Address start_, 
-	       Dyninst::ParseAPI::CodeObject* o,
-	       Dyninst::ParseAPI::CodeRegion* r,
-	       Dyninst::InstructionSource *isrc,
-	       Dyninst::ParseAPI::Block * curBlk_):
-	           IA_IAPI(dec_, start_, o, r, isrc, curBlk_) {
-}		   
-IA_aarch64::IA_aarch64(const IA_aarch64& rhs): IA_IAPI(rhs) {}
+IA_aarch64::IA_aarch64(Dyninst::InstructionAPI::InstructionDecoder dec_, Address start_,
+                       Dyninst::ParseAPI::CodeObject* o, Dyninst::ParseAPI::CodeRegion* r,
+                       Dyninst::InstructionSource* isrc, Dyninst::ParseAPI::Block* curBlk_)
+    : IA_IAPI(dec_, start_, o, r, isrc, curBlk_) {}
+
+IA_aarch64::IA_aarch64(const IA_aarch64& rhs) : IA_IAPI(rhs) {}
 
 IA_aarch64* IA_aarch64::clone() const {
-    return new IA_aarch64(*this);
+  return new IA_aarch64(*this);
 }
 
-bool IA_aarch64::isFrameSetupInsn(Instruction i) const
-{
-    if(i.getOperation().getID() == aarch64_op_mov_add_addsub_imm)
-    {
-	if(i.readsMemory() || i.writesMemory())
-	{
-	    parsing_printf("%s[%d]: discarding insn %s as stack frame preamble, not a reg-reg move\n", FILE__, __LINE__,
-                       i.format().c_str());
+bool IA_aarch64::isFrameSetupInsn(Instruction i) const {
+  if(i.getOperation().getID() == aarch64_op_mov_add_addsub_imm) {
+    if(i.readsMemory() || i.writesMemory()) {
+      parsing_printf("%s[%d]: discarding insn %s as stack frame preamble, not a reg-reg move\n", FILE__, __LINE__,
+                     i.format().c_str());
 
-	    return false;
-	}
-	if(i.isRead(stackPtr[_isrc->getArch()]) && i.isWritten(framePtr[_isrc->getArch()]))
-	{
-	    return true;
-	}
+      return false;
     }
-    return false;
-}
-
-bool IA_aarch64::isNop() const
-{
-    Instruction ci = curInsn();
-
-    if(ci.getOperation().getID() == aarch64_op_nop_hint)
-	return true;
-
-    return false;
-}
-
-bool IA_aarch64::isThunk() const 
-{
-    return false;
-}
-
-bool IA_aarch64::isTailCall(const Function* context, EdgeTypeEnum type, unsigned int,
-        const std::set<Address>& knownTargets ) const
-{
-    switch(type) {
-       case CALL:
-       case COND_TAKEN:
-       case DIRECT:
-       case INDIRECT:
-          type = DIRECT;
-          break;
-       case COND_NOT_TAKEN:
-       case FALLTHROUGH:
-       case CALL_FT:
-       case RET:
-       default:
-          return false;
-    }
-
-    parsing_printf("Checking for Tail Call from ARM\n");
-    context->obj()->cs()->incrementCounter(PARSE_TAILCALL_COUNT); 
-
-    if (tailCalls.find(type) != tailCalls.end()) {
-        parsing_printf("\tReturning cached tail call check result: %d\n", tailCalls[type]);
-        if (tailCalls[type]) {
-            context->obj()->cs()->incrementCounter(PARSE_TAILCALL_FAIL);
-            return true;
-        }
-        return false;
-    }
-    
-    bool valid; Address addr;
-    boost::tie(valid, addr) = getCFT();
-
-    Function *callee = _obj->findFuncByEntry(_cr, addr);
-    Block *target = _obj->findBlockByEntry(_cr, addr);
-
-    if(curInsn().isBranch() &&
-       valid &&
-       callee && 
-       callee != context &&
-       // We can only trust entry points from hints
-       callee->src() == HINT &&
-       /* the target can either be not parsed or not within the current context */
-       ((target == NULL) || (target && !context->contains(target)))
-       )
-    {
-      parsing_printf("\tjump to 0x%lx, TAIL CALL\n", addr);
-      tailCalls[type] = true;
+    if(i.isRead(stackPtr[_isrc->getArch()]) && i.isWritten(framePtr[_isrc->getArch()])) {
       return true;
     }
-
-    if (valid && addr > 0 && !context->region()->contains(addr)) {
-      parsing_printf("\tjump to 0x%lx in other regions, TAIL CALL\n", addr);
-      tailCalls[type] = true;
-      return true;
-    }    
-
-    if (curInsn().isBranch() &&
-            valid &&
-            !callee) {
-    if (knownTargets.find(addr) != knownTargets.end()) {
-	    parsing_printf("\tjump to 0x%lx is known target in this function, NOT TAIL CALL\n", addr);
-	    tailCalls[type] = false;
-	    return false;
-	}
-    }
-
-
-
-    if(allInsns.size() < 2) {
-        parsing_printf("\ttoo few insns to detect tail call\n");
-        context->obj()->cs()->incrementCounter(PARSE_TAILCALL_FAIL);
-        tailCalls[type] = false;
-        return false;
-    }
-    tailCalls[type] = false;
-    context->obj()->cs()->incrementCounter(PARSE_TAILCALL_FAIL);
-    return false;
-}
-
-bool IA_aarch64::savesFP() const
-{
-    Instruction insn = curInsn();
-    RegisterAST::Ptr returnAddrReg(new RegisterAST(aarch64::x30));
-
-    //stp x29, x30, [sp, imm]!
-    if(insn.getOperation().getID() == aarch64_op_stp_gen &&
-       insn.isRead(framePtr[_isrc->getArch()]) &&
-       insn.isRead(returnAddrReg) &&
-       insn.isRead(stackPtr[_isrc->getArch()]) &&
-       insn.isWritten(stackPtr[_isrc->getArch()]))
-        return true;
-
-    return false;
-}
-
-bool IA_aarch64::isStackFramePreamble() const
-{
-    if(!savesFP())
-	return false;
-
-    InstructionDecoder tmp(dec);
-    if(isFrameSetupInsn(tmp.decode()))
-        return true;
-
-    return false;
-}
-
-bool IA_aarch64::cleansStack() const
-{
-    Instruction insn = curInsn();
-    RegisterAST::Ptr returnAddrReg(new RegisterAST(aarch64::x30));
-
-    //ldp x29, x30, [sp], imm
-    if(insn.getOperation().getID() == aarch64_op_ldp_gen &&
-       insn.isWritten(framePtr[_isrc->getArch()]) &&
-       insn.isWritten(returnAddrReg) &&
-       insn.isRead(stackPtr[_isrc->getArch()]) &&
-       insn.isWritten(stackPtr[_isrc->getArch()]))
-        return true;
-
-    return false;
-}
-
-bool IA_aarch64::sliceReturn(ParseAPI::Block*, Address, ParseAPI::Function *) const
-{
-    return true;
-}
-
-bool IA_aarch64::isReturnAddrSave(Address&) const
-{
+  }
   return false;
 }
 
-bool IA_aarch64::isReturn(Dyninst::ParseAPI::Function *, Dyninst::ParseAPI::Block*) const
-{
-    return curInsn().isReturn();
+bool IA_aarch64::isNop() const {
+  Instruction ci = curInsn();
+
+  if(ci.getOperation().getID() == aarch64_op_nop_hint) {
+    return true;
+  }
+
+  return false;
 }
 
-bool IA_aarch64::isFakeCall() const
-{
+bool IA_aarch64::isThunk() const {
+  return false;
+}
+
+bool IA_aarch64::isTailCall(const Function* context, EdgeTypeEnum type, unsigned int,
+                            const std::set<Address>& knownTargets) const {
+  switch(type) {
+    case CALL:
+    case COND_TAKEN:
+    case DIRECT:
+    case INDIRECT:
+      type = DIRECT;
+      break;
+    case COND_NOT_TAKEN:
+    case FALLTHROUGH:
+    case CALL_FT:
+    case RET:
+    default:
+      return false;
+  }
+
+  parsing_printf("Checking for Tail Call from ARM\n");
+  context->obj()->cs()->incrementCounter(PARSE_TAILCALL_COUNT);
+
+  if(tailCalls.find(type) != tailCalls.end()) {
+    parsing_printf("\tReturning cached tail call check result: %d\n", tailCalls[type]);
+    if(tailCalls[type]) {
+      context->obj()->cs()->incrementCounter(PARSE_TAILCALL_FAIL);
+      return true;
+    }
     return false;
-}
+  }
 
-bool IA_aarch64::isIATcall(std::string &) const
-{
+  bool valid;
+  Address addr;
+  boost::tie(valid, addr) = getCFT();
+
+  Function* callee = _obj->findFuncByEntry(_cr, addr);
+  Block* target = _obj->findBlockByEntry(_cr, addr);
+
+  if(curInsn().isBranch() && valid && callee && callee != context &&
+     // We can only trust entry points from hints
+     callee->src() == HINT &&
+     /* the target can either be not parsed or not within the current context */
+     ((target == NULL) || (target && !context->contains(target)))) {
+    parsing_printf("\tjump to 0x%lx, TAIL CALL\n", addr);
+    tailCalls[type] = true;
+    return true;
+  }
+
+  if(valid && addr > 0 && !context->region()->contains(addr)) {
+    parsing_printf("\tjump to 0x%lx in other regions, TAIL CALL\n", addr);
+    tailCalls[type] = true;
+    return true;
+  }
+
+  if(curInsn().isBranch() && valid && !callee) {
+    if(knownTargets.find(addr) != knownTargets.end()) {
+      parsing_printf("\tjump to 0x%lx is known target in this function, NOT TAIL CALL\n", addr);
+      tailCalls[type] = false;
+      return false;
+    }
+  }
+
+  if(allInsns.size() < 2) {
+    parsing_printf("\ttoo few insns to detect tail call\n");
+    context->obj()->cs()->incrementCounter(PARSE_TAILCALL_FAIL);
+    tailCalls[type] = false;
     return false;
+  }
+  tailCalls[type] = false;
+  context->obj()->cs()->incrementCounter(PARSE_TAILCALL_FAIL);
+  return false;
 }
 
-bool IA_aarch64::isLinkerStub() const
-{
+bool IA_aarch64::savesFP() const {
+  Instruction insn = curInsn();
+  RegisterAST::Ptr returnAddrReg(new RegisterAST(aarch64::x30));
+
+  // stp x29, x30, [sp, imm]!
+  if(insn.getOperation().getID() == aarch64_op_stp_gen && insn.isRead(framePtr[_isrc->getArch()]) &&
+     insn.isRead(returnAddrReg) && insn.isRead(stackPtr[_isrc->getArch()]) &&
+     insn.isWritten(stackPtr[_isrc->getArch()])) {
+    return true;
+  }
+
+  return false;
+}
+
+bool IA_aarch64::isStackFramePreamble() const {
+  if(!savesFP()) {
+    return false;
+  }
+
+  InstructionDecoder tmp(dec);
+  if(isFrameSetupInsn(tmp.decode())) {
+    return true;
+  }
+
+  return false;
+}
+
+bool IA_aarch64::cleansStack() const {
+  Instruction insn = curInsn();
+  RegisterAST::Ptr returnAddrReg(new RegisterAST(aarch64::x30));
+
+  // ldp x29, x30, [sp], imm
+  if(insn.getOperation().getID() == aarch64_op_ldp_gen && insn.isWritten(framePtr[_isrc->getArch()]) &&
+     insn.isWritten(returnAddrReg) && insn.isRead(stackPtr[_isrc->getArch()]) &&
+     insn.isWritten(stackPtr[_isrc->getArch()])) {
+    return true;
+  }
+
+  return false;
+}
+
+bool IA_aarch64::sliceReturn(ParseAPI::Block*, Address, ParseAPI::Function*) const {
+  return true;
+}
+
+bool IA_aarch64::isReturnAddrSave(Address&) const {
+  return false;
+}
+
+bool IA_aarch64::isReturn(Dyninst::ParseAPI::Function*, Dyninst::ParseAPI::Block*) const {
+  return curInsn().isReturn();
+}
+
+bool IA_aarch64::isFakeCall() const {
+  return false;
+}
+
+bool IA_aarch64::isIATcall(std::string&) const {
+  return false;
+}
+
+bool IA_aarch64::isLinkerStub() const {
   // Disabling this code because it ends with an
-    // incorrect CFG.
-    return false;
+  // incorrect CFG.
+  return false;
 }
 
-bool IA_aarch64::isNopJump() const
-{
-    return false;
+bool IA_aarch64::isNopJump() const {
+  return false;
 }
