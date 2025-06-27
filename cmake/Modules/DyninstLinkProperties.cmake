15f158f4d7bd78ebe908ebd3a8caffd46b944e8e
Tim Haines <thaines.astro@gmail.com>
2025-06-26 20:09:41 -0500

    CMake: update detection of static libs -- PART 2

--- cmake/Modules/DyninstLinkProperties.cmake
+++ cmake/Modules/DyninstLinkProperties.cmake
@@ -4,6 +4,13 @@ include(CheckCCompilerFlag)
 include(CheckCXXCompilerFlag)
 
 # Check if the CMAKE_<LANG>_COMPILER supports static linking
+#
+# NOTE: This is only needed when creating executables because
+#
+#         add_executable(foo STATIC foo.cpp)
+#
+#       doesn't (currently) work. Libraries should use the STATIC
+#       keyword as usual.
 
 # gcc and clang both use '-static', but allow for other compilers
 set(_possible_static_flags "-static")
@@ -27,36 +34,6 @@ foreach(_flag ${_possible_static_flags})
     set(DYNINST_CXX_STATIC_LINK_FLAG
         "${_flag}"
         CACHE STRING "C++ static link flag" FORCE)
-  endif()
-
-  set(CMAKE_REQUIRED_LINK_OPTIONS "${_saved_link_opts}")
-endforeach()
-
-##############################################################################
-
-# Check if the CMAKE_<LANG>_COMPILER supports a 32-bit runtime
-
-# gcc and clang both use 'm32', but allow for other compilers
-set(_possible_32bit_flags "-m32")
-
-foreach(_flag ${_possible_32bit_flags})
-
-  message(STATUS "testing flag '${_flag}'")
-
-  set(_saved_link_opts ${CMAKE_REQUIRED_LINK_OPTIONS})
-
-  set(CMAKE_REQUIRED_LINK_OPTIONS "${_flag}")
-
-  check_c_compiler_flag("${_flag}" DYNINST_C_HAVE_32BIT_LINK_FLAG)
-
-  if(DYNINST_C_HAVE_32BIT_LINK_FLAG AND NOT DYNINST_C_32BIT_LINK_FLAG)
-    set(DYNINST_C_32BIT_LINK_FLAG "${_flag}")
-  endif()
-
-  check_cxx_compiler_flag("${_flag}" DYNINST_CXX_HAVE_32BIT_LINK_FLAG)
-
-  if(DYNINST_CXX_HAVE_32BIT_LINK_FLAG AND NOT DYNINST_CXX_32BIT_LINK_FLAG)
-    set(DYNINST_CXX_32BIT_LINK_FLAG "${_flag}")
   endif()
 
   set(CMAKE_REQUIRED_LINK_OPTIONS "${_saved_link_opts}")
