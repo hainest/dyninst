1680fc03f193b1aecbcc1643b59d43fab436d0aa
Tim Haines <thaines.astro@gmail.com>
2026-04-23 19:57:09 -0500

    Add Dyninst::regex

---
+++ common/src/dyninst_regex.h
@@ -0,0 +1,67 @@
+/*
+ * See the dyninst/COPYRIGHT file for copyright information.
+ *
+ * We provide the Paradyn Tools (below described as "Paradyn")
+ * on an AS IS basis, and do not warrant its validity or performance.
+ * We reserve the right to update, modify, or discontinue this
+ * software at any time.  We shall have no obligation to supply such
+ * updates or modifications or any other form of support to you.
+ *
+ * By your use of Paradyn, you understand and agree that we (or any
+ * other person or entity with proprietary rights in Paradyn) are
+ * under no obligation to provide either maintenance services,
+ * update services, notices of latent defects, or correction of
+ * defects for Paradyn.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+
+#ifndef DYNINST_COMMON_REGEX_H
+#define DYNINST_COMMON_REGEX_H
+
+#include <boost/regex.hpp>
+#include <string>
+
+
+namespace Dyninst {
+
+  namespace detail {
+
+    struct bool_opt {
+      const bool val{false};
+      constexpr explicit operator bool() const {
+        return val;
+      }
+    };
+
+  }
+
+  class regex final {
+  public:
+
+    struct icase final : detail::bool_opt {};
+
+    regex(std::string) {}
+
+  private:
+    std::string text;
+    boost::regex::flag_type flags{};
+
+  };
+
+}
+
+#endif
